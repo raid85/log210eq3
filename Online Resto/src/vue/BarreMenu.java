b696fb21da58e1ddcc9ccbf46fc7476119dcc221
shadamon84 <shadamon84@d26188fc-ba83-11de-8be4-e1425cda3908>
2009-11-24 14:55:23 +0000

    Suppression de l'atttibut heureFermeture car ne se trouve pas dans les exigences

--- Online Resto/src/vue/BarreMenu.java
+++ Online Resto/src/vue/BarreMenu.java
@@ -199,7 +199,7 @@ public class BarreMenu extends JMenuBar implements Observer{
 	}
 	
 	public void nouvelleVueClient(){
-		panneauTempo = new VueClient();
+		panneauTempo = new VueRestaurant();
 		
 	}
 	public void nouvelleVueMenu(){
